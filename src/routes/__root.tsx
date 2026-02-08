c4fb513179401efe45dd230d87b0980f34fdd4b0
jankii03 <januel.torres@upr.edu>
2026-02-08 11:55:33 -0400

    Updated tab icon to be official logo.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -33,6 +33,11 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 		],
 		links: [
 			{
+				rel: "icon",
+				type: "image/png",
+				href: "/aura-hype-official-logo.png",
+			},
+			{
 				rel: "stylesheet",
 				href: appCss,
 			},
